e2bf5cadeea550625c2e39b1323988faeaa74df9
soliman elamsry <solyana38@gmail.com>
2022-12-20 04:22:08 +0200

    contenue responsive design 1

--- src/vezzeta_project/static/js/main.js
+++ src/vezzeta_project/static/js/main.js
@@ -1,1 +1,12 @@
-console.log("frrom  javvascript fiile")
+window.onload=function(){
+
+    let btn = document.getElementById("burger")
+    let ul = document.getElementById('ul')
+    console.log(btn)
+
+    btn.onclick = ()=>{
+        btn.classList.toggle("active")
+        ul.classList.toggle("active")
+    }
+    
+  }
